a84bfb964fb99fd92e501503132ffdac6a893bd0
Alexander Golin <agolin95@gmail.com>
2024-02-22 13:01:37 -0500

    better scrolling

--- js/planet.js
+++ js/planet.js
@@ -47,10 +47,9 @@ class Planet {
 
         let element = this.id;
         $(`.${this.id}.sidebarplanet`).click(function () {
-            // document.getElementById(element).scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
             let target = $(`#${element}`)
             $('html').animate({
-                scrollTop: target.offset().top - (window.screen.height / 2.5)
+                scrollTop: target.offset().top - ($(window).height() / 2.5)
             }, 2000);
         });
     }
