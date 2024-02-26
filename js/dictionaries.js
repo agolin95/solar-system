f34040453a29043c0264b75afa51b2c962231cfb
Alexander Golin <agolin95@gmail.com>
2024-02-26 00:52:48 -0500

    refactor into dictionaries for programmatic class generation

--- js/dictionaries.js
+++ js/dictionaries.js
@@ -49,20 +49,271 @@ const planets = {
     },
     "Neptune": {
         "radius": 24622,
-        "orbit": 30,
+        "orbit": 30.07,
         "color": "#446afc"
-
     }
 }
 
-const dwarves = {
 
+const dwarves = {
+    "Ceres": {
+        "radius": 469.7,
+        "orbit": 2.76596
+    },
+    "Orcus": {
+        "radius": 917,
+        "orbit": 39.174
+    },
+    "Pluto": {
+        "radius": 1151,
+        "orbit": 39.5294
+    },
+    "Salacia": {
+        "radius": 854,
+        "orbit": 42.184
+    },
+    "Varuna": {
+        "radius": 668,
+        "orbit": 42.718
+    },
+    "Haumea": {
+        "radius": 780,
+        "orbit": 43.335
+    },
+    "Quaoar": {
+        "radius": 543,
+        "orbit": 43.694
+    },
+    "Makemake": {
+        "radius": 715,
+        "orbit": 45.791
+    },
+    "Gonggong": {
+        "radius": 615,
+        "orbit": 67.5
+    },
+    "Eris": {
+        "radius": 1163,
+        "orbit": 67.6681
+    },
+    // "Sedna": {
+    //     "radius": 995,
+    //     "orbit": 506
+    // }
 }
+
 
 const moons = {
-
+    "Earth": {
+        "Luna": {
+            "radius": 1737.5,
+            "semi-major-axis": 384748
+        },
+    },
+    "Mars": {
+        "Phobos": {
+            "radius": 11.1,
+            "semi-major-axis": 9377
+        },
+        "Deimos": {
+            "radius": 6.3,
+            "semi-major-axis": 23460
+        },
+    },
+    "Jupiter": {
+        "Io": {
+            "radius": 1821.6,
+            "semi-major-axis": 421800
+        },
+        "Europa": {
+            "radius": 1560.8,
+            "semi-major-axis": 671100
+        },
+        "Ganymede": {
+            "radius": 2634.1,
+            "semi-major-axis": 1070400
+        },
+        "Callisto": {
+            "radius": 2410.3,
+            "semi-major-axis": 1882700
+        },
+    },
+    "Saturn": {
+        "Titan": {
+            "planet": "Saturn",
+            "radius": 2574.73,
+            "semi-major-axis": 1221900
+        },
+        "Rhea": {
+            "planet": "Saturn",
+            "radius": 763.8,
+            "semi-major-axis": 527200
+        },
+        "Iapetus": {
+            "planet": "Saturn",
+            "radius": 734.3,
+            "semi-major-axis": 3561700
+        },
+        "Dione": {
+            "planet": "Saturn",
+            "radius": 561.4,
+            "semi-major-axis": 377700
+        },
+        "Tethys": {
+            "planet": "Saturn",
+            "radius": 531.1,
+            "semi-major-axis": 295000
+        },
+        "Enceladus": {
+            "planet": "Saturn",
+            "radius": 252.1,
+            "semi-major-axis": 238400
+        },
+        "Mimas": {
+            "planet": "Saturn",
+            "radius": 198.2,
+            "semi-major-axis": 186000
+        },
+    },
+    "Uranus": {
+        "Titania": {
+            "planet": "Uranus",
+            "radius": 788.4,
+            "semi-major-axis": 435910
+        },
+        "Oberon": {
+            "planet": "Uranus",
+            "radius": 761.4,
+            "semi-major-axis": 583520
+        },
+        "Umbriel": {
+            "planet": "Uranus",
+            "radius": 584.7,
+            "semi-major-axis": 266300
+        },
+        "Ariel": {
+            "planet": "Uranus",
+            "radius": 578.9,
+            "semi-major-axis": 191020
+        },
+        "Miranda": {
+            "planet": "Uranus",
+            "radius": 235.8,
+            "semi-major-axis": 129390
+        },
+    },
+    "Neptune": {
+        "Triton": {
+            "planet": "Neptune",
+            "radius": 1352.6,
+            "semi-major-axis": 354759
+        },
+    },
+    "Pluto": {
+        "Charon": {
+            "planet": "Pluto",
+            "radius": 606,
+            "semi-major-axis": 17536
+        },
+    },
 }
 
+
 const boundaries = {
+    "Asteroid-Belt-Start": {
+        "orbit": 2.1
+    },
+    "Asteroid-Belt-End": {
+        "orbit": 3.3
+    },
+    "Kuiper-Belt-Start": {
+        "orbit": 30
+    },
+    "Kuiper-Belt-End": {
+        "orbit": 50
+    },
+    "Heliosheath": {
+        "orbit": 90
+    },
+    // "Heliopause": {
+    //     "orbit": 150
+    // },
+    // "Bow-Shock": {
+    //     "orbit": 250
+    // },
+}
+
+
+const saturnRingColor = "#a07535";
+const saturnOuterColor = "rgba(255,255,255,0.08)";
+const gapColor = "black";
+const rings = {
+    "Saturn": {
+        "Ring-E": {
+            "radius": 480000,
+            "color": saturnOuterColor
+        },
+        "Huygens-Gap": {
+            "radius": 173500,
+            "color": gapColor
+        },
+        "Ring-G": {
+            "radius": 167500,
+            "color": saturnOuterColor
+        },
+        "Maxwell-Gap": {
+            "radius": 141100,
+            "color": gapColor
+        },
+        "Ring-F": {
+            "radius": 140180,
+            "color": saturnOuterColor
+        },
+        "Colombo-Gap": {
+            "radius": 139840,
+            "color": gapColor
+        },
+        "Colombo-Filler": {
+            "radius": 139330,
+            "color": saturnRingColor
+        },
+        "Keeler-Gap": {
+            "radius": 137300,
+            "color": gapColor
+        },
+        "Ring-A": {
+            "radius": 136500,
+            "color": saturnRingColor
+        },
+        "Encke-Gap": {
+            "radius": 133780,
+            "color": gapColor
+        },
+        "Encke-Filler": {
+            "radius": 133600,
+            "color": saturnRingColor
+        },
+        "Cassini-Division": {
+            "radius": 122200,
+            "color": gapColor
+        },
+        "Ring-B": {
+            "radius": 117500,
+            "color": saturnRingColor
+        },
+        "Ring-C": {
+            "radius": 92000,
+            "color": saturnRingColor
+        },
+        "Ring-D": {
+            "radius": 74500,
+            "color": saturnRingColor
+        },
+        "Ring-Gap": {
+            "radius": 66900,
+            "color": gapColor
+        },
+
+    }
 
 }
--- js/main.js
+++ js/main.js
@@ -1,105 +1,46 @@
 $(function () {
+    // Draw Rings
+    for (const [key, value] of Object.entries(rings)) {
+        for (const [k, v] of Object.entries(value)) {
+            const planet = planets[key];
+            const ring = new Ring(k, v["radius"], planet["orbit"], v["color"]);
+            ring.draw();
+        }
+    }
 
+    // Draw Planets
     for (const [key, value] of Object.entries(planets)) {
-        let planet = new Planet(key, value["radius"], value["orbit"], value["color"]);
+        const planet = new Planet(key, value["radius"], value["orbit"], value["color"]);
         planet.draw();
     }
 
-    let moonColor = "#c0bebf";
 
-    // let sun = new Planet("Sun", 695508, 0, "#fda835").draw();
-
-    // let mercury = new Planet("Mercury", 2439.7, .3871, "#ad8a6c").draw();
-
-    // let venus = new Planet("Venus", 6051.8, .7233, "#ca6b16").draw();
-
-    let earthOrbit = 1;
-    // let earth = new Planet("Earth", 6371, earthOrbit, "#38569a").draw();
-    let luna = new Moon("Luna", 1737.5, earthOrbit - 0.002569544, moonColor).draw();
-
-    // let mars = new Planet("Mars", 3389.5, 1.5273, "#b92e1a").draw();
-    // let phobos = new Moon("Phobos", 11, 1.5273626747, moonColor).draw();
-    // let deimos = new Moon("Deimos", 6.2, 1.5274568418, moonColor).draw();
-
-    let asteroidstart = new Boundary("Asteroid-Belt-Start", 0, 2.1, "white").draw();
-    let ceres = new Dwarf("Ceres", 469.7, 2.76596, "#ccc").draw();
-    let asteroidend = new Boundary("Asteroid-Belt-End", 0, 3.3, "white").draw();
+    // Draw Dwarves
+    for (const [key, value] of Object.entries(dwarves)) {
+        const dwarf = new Dwarf(key, value["radius"], value["orbit"], "#ccc");
+        dwarf.draw();
+    }
 
 
-    let jupiterOrbit = 5.2028;
-    // let jupiter = new Planet("Jupiter", 69911, jupiterOrbit, "#bc6328").draw();
-    let io = new Moon("Io", 1821.6, jupiterOrbit - 0.002819559, moonColor).draw();
-    let europa = new Moon("Europa", 1560.8, jupiterOrbit - 0.004486026, moonColor).draw();
-    let ganymede = new Moon("Ganymede", 2634.1, jupiterOrbit - 0.007155182, moonColor).draw();
-    let callisto = new Moon("Callisto", 2410.3, jupiterOrbit - 0.01258507, moonColor).draw();
+    // Draw Moons
+    for (const [key, value] of Object.entries(moons)) {
+        for (const [k, v] of Object.entries(value)) {
+            const orbitInAU = v["semi-major-axis"] / 149597871;
+            const planet = planets[key] || dwarves[key];
+            const moon = new Moon(key, v["radius"], planet["orbit"] - orbitInAU, "#c0bebf");
+            moon.draw();
+        }
+    }
 
 
-    let saturnOrbit = 9.5388;
-    let saturnRingColor = "#a07535";
-    let saturnOuterColor = "rgba(255,255,255,0.08)";
-    let saturnringe = new Ring("Saturn-Ring-E", 480000, saturnOrbit, saturnOuterColor).draw();
-    let huygensgap = new Ring("Huygens-Gap", 173500, saturnOrbit, "black").draw();
-    let saturnringg = new Ring("Saturn-Ring-G", 167500, saturnOrbit, saturnOuterColor).draw();
-    let maxwellgap = new Ring("Maxwell-Gap", 141100, saturnOrbit, "black").draw();
-    let maxwellfiller = new Ring("Maxwell-Filler", 140900, saturnOrbit, saturnRingColor).draw();
-    let saturnringf = new Ring("Saturn-Ring-F", 140180, saturnOrbit, saturnOuterColor).draw();
-    let colombogap = new Ring("Colombo-Gap", 139800, saturnOrbit, "black").draw();
-    let colombofiller = new Ring("Colombo-Filler", 13400, saturnOrbit, saturnRingColor).draw();
-    let keelergap = new Ring("Keeler-Gap", 137300, saturnOrbit, "black").draw();
-    let saturnringa = new Ring("Saturn-Ring-A", 136500, saturnOrbit, saturnRingColor).draw();
-    let enckegap = new Ring("Encke-Gap", 133780, saturnOrbit, "black").draw();
-    let enckefiller = new Ring("Encke-Filler", 133600, saturnOrbit, saturnRingColor).draw();
-    let cassinidivision = new Ring("Cassini-Division", 122200, saturnOrbit, "black").draw();
-    let saturnringb = new Ring("Saturn-Ring-B", 117500, saturnOrbit, saturnRingColor).draw();
-    let saturnringc = new Ring("Saturn-Ring-C", 92000, saturnOrbit, saturnRingColor).draw();
-    let saturnringd = new Ring("Saturn-Ring-D", 74500, saturnOrbit, saturnRingColor).draw();
-    let saturngap = new Ring("Saturn-Gap", 66900, saturnOrbit, "black").draw();
-    // let saturn = new Planet("Saturn", 58232, saturnOrbit, "#fdca95").draw();
-    let titan = new Moon("Titan", 5149.46 / 2, saturnOrbit - 0.008167897, moonColor).draw();
-    let rhea = new Moon("Rhea", 1527.6 / 2, saturnOrbit - 0.003524114, moonColor).draw();
-    let iapetus = new Moon("Iapetus", 1468.6 / 2, saturnOrbit - 0.02380849, moonColor).draw();
-    let dione = new Moon("Dione", 1122 / 2, saturnOrbit - 0.002524769, moonColor).draw();
-    let tethys = new Moon("Tethys", 1062.2 / 2, saturnOrbit - 0.001971953, moonColor).draw();
-    let enceladus = new Moon("Enceladus", 504.2 / 2, saturnOrbit - 0.001593606, moonColor).draw();
-    let mimas = new Moon("Mimas", 396.4 / 2, saturnOrbit - 0.001243333, moonColor).draw();
-
-    let uranusOrbit = 19.1914;
-    // let uranus = new Planet("Uranus", 25362, uranusOrbit, "#87d7de").draw();
-    let titania = new Moon("Titania", 1576.8 / 2, uranusOrbit - 0.002913878, moonColor).draw();
-    let oberon = new Moon("Oberon", 1522.8 / 2, uranusOrbit - 0.00390059, moonColor).draw();
-    let umbriel = new Moon("Umbriel", 1169.4 / 2, uranusOrbit - 0.001780106, moonColor).draw();
-    let ariel = new Moon("Ariel", 1157.8 / 2, uranusOrbit - 0.00127689, moonColor).draw();
-    let miranda = new Moon("Miranda", 471.6 / 2, uranusOrbit - 0.0008649187, moonColor).draw();
-
-    let neptuneOrbit = 30;
-    // let neptune = new Planet("Neptune", 24622, neptuneOrbit, "#446afc").draw();
-    let triton = new Moon("Triton", 2705.2 / 2, neptuneOrbit - 0.002371417, moonColor).draw();
+    // Draw Boundaries
+    for (const [key, value] of Object.entries(boundaries)) {
+        const boundary = new Boundary(key, 0, value["orbit"], "white");
+        boundary.draw();
+    }
 
 
-    let kuiperstart = new Boundary("Kuiper-Belt-Start", 0, 30, "white").draw();
-
-    let orcus = new Dwarf("Orcus", 917, 39.174, "#ccc").draw();
-
-    let plutoOrbit = 39.5294;
-    let pluto = new Dwarf("Pluto", 1151, plutoOrbit, "#ccc").draw();
-    // let charon = new Moon("Charon", 1212 / 2, plutoOrbit - 0.0001172209, moonColor).draw();
-
-    let salacia = new Dwarf("Salacia", 854, 42.184, "#ccc").draw();
-    let varuna = new Dwarf("Varuna", 668, 42.718, "#ccc").draw();
-    let haumea = new Dwarf("Haumea", 780, 43.335, "#ccc").draw();
-    let quaoar = new Dwarf("Quaoar", 543, 43.694, "#ccc").draw();
-    let makemake = new Dwarf("Makemake", 715, 45.791, "#ccc").draw();
-
-    let kuiperend = new Boundary("Kuiper-Belt-End", 0, 50, "white").draw();
-
-    let gonggong = new Dwarf("Gonggong", 615, 67.5, "#ccc").draw();
-    let eris = new Dwarf("Eris", 1163, 67.6681, "#ccc").draw();
-    // let sedna = new Dwarf("Sedna", 995, 506, "#ccc").draw();
-
-    let heliosheath = new Boundary("Heliosheath", 0, 90, "white").draw();
-    // let heliopause = new Boundary("Heliopause", 0, 150, "white").draw();
-    // let bowshock = new Boundary("Bow-Shock", 0, 250, "white").draw();
-
+    // Add Music Controls
     $(".music-control").on("click", function () {
         const elem = document.getElementById("nightbus");
         elem.paused ? elem.play() : elem.pause();
